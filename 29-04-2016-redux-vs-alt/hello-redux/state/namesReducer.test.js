908f81dc2d9f65ef1777d9a519ab7dde94a1aaf8
Uldis Sturms <uldis.sturms@gmail.com>
2016-04-26 23:41:53 +0100

    store tests no longer make sense now that we have that logic covered by end-to-end tests

--- 29-04-2016-redux-vs-alt/hello-redux/state/namesReducer.test.js
+++ 29-04-2016-redux-vs-alt/hello-redux/state/namesReducer.test.js
@@ -2,10 +2,14 @@ import expect from 'expect';
 import namesReducer from './namesReducer';
 
 const greet = name => {
-  return { type: 'GREET', payload: { name: 'Stranger' }};
+  return { type: 'GREET', payload: { name: name }};
 };
 
 describe('names reducer', () => {
+  it('greets', () => {
+    const state = namesReducer({ names: [] }, greet('Steve'));
+    expect(state).toEqual({ names: ['Steve']});
+  });
   it('no greetings to Stranger', () => {
     const state = namesReducer({ names: [] }, greet('Stranger'));
     expect(state).toEqual({ names: []});
